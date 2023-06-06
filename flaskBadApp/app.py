9664d9986b7e8a508f75d6602df9f0ccb57aeace
PythonDrinkCoffee <bartekbarszczewski29@outlook.com>
2023-06-06 19:43:55 +0200

    test flaskBadApp nr 9

--- flaskBadApp/app.py
+++ flaskBadApp/app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 app.secret_key = secrets.token_urlsafe(32)
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET', "POST"])
 def index():
+
     return render_template('base.html')
 
 
